86eac89a36d89e7a3b8d3717d478cdf556510df3
BretonLud <godneo@hotmail.fr>
2024-10-03 21:36:10 +0200

    Add Docker configurations for Nginx, MariaDB, and PHP services
    Introduce configuration files for Nginx, MariaDB, and PHP, including customized settings and dependencies. Update .env file with necessary environment variables, and set up Docker Compose services with appropriate volumes, networks, and dependencies.

---
+++ entrypoint.sh
@@ -0,0 +1,14 @@
+# Boucle pour exécuter la migration jusqu'à ce qu'elle se termine avec succès
+while true; do
+  php bin/console d:m:m --no-interaction && break
+  # Attend quelques secondes entre les tentatives
+  sleep 5s
+done
+
+php bin/console cache:clear
+chown -R www-data:www-data /var/www/var
+
+php bin/console lexik:jwt:generate-keypair --skip-if-exists
+
+# Une fois la migration réussie, démarrez php-fpm
+php-fpm
